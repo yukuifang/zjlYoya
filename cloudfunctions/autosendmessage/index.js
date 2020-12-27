d1448f9078431319c47a1d4951115cd37a58b55c
fangyukui <m13560050110@163.com>
2020-12-27 21:00:44 +0800

    77

--- cloudfunctions/autosendmessage/index.js
+++ cloudfunctions/autosendmessage/index.js
@@ -12,9 +12,9 @@ exports.main = async (event, context) => {
   //获取明天的时间
  var day = new Date();
  day.setTime(day.getTime()+24*60*60*1000)
- workdate = getYYMMDD(day)
+ var workdate = getYYMMDD(day)
 
- // 获取所有老师的明天计划
+ // 获取所有老师的明天计划推送给其他用户
  var schedules = await scheduleCollection
     .where({
       workdate
@@ -63,44 +63,50 @@ exports.main = async (event, context) => {
         }
     } 
 
-  }  
+  }
+  
+  
+  // 获取今天的时间
+  var day = new Date();
+  var today_workdate = getYYMMDD(day)
+
+  // 获取所有老师的今天的计划
+ var today_schedules = await scheduleCollection
+ .where({
+   workdate:today_workdate
+ })
+ .get()
+ .then(res => {
+   return res.data
+ })
 
 
-  return '推送成功'
+ if(today_schedules!=undefined && today_schedules.length >0){
+  for(var i = 0; i< today_schedules.length;i++){
+      var schedule = today_schedules[i]
+      if(schedule.lessions!=undefined && schedule.lessions.length>0){
+          for(var j = 0;j< schedule.lessions.length;j++){
+            var tmp =  schedule.lessions[j]
+            if(tmp.is_sigin_in!=undefined && tmp.is_sigin_in == true){
+              continue
+            }
+            tmp.is_sigin_in = true
+            await scheduleCollection
+            .doc(schedule._id)
+            .update({
+              data:{
+                lessions:schedule.lessions
+              }
+            })
+            .then(res=>{
+              return res.data
+            })
+          }
 
-   
-
-
-
-
-  // const result = await cloud.openapi.subscribeMessage.send({
-  //   touser: 'opIkL0e3d7sBuNCggrLkh92jqp_A', //要推送给那个用户
-  //   page: 'pages/mine/mine', //要跳转到那个小程序页面
-  //   lang: 'zh_CN',
-  //   data: { //推送的内容
-  //     name1: {
-  //       value: 'aa'
-  //     },
-  //     name10: {
-  //       value: "bb"
-  //     },
-  //     thing2: {
-  //       value: "cc"
-  //     },
-  //     date3: {
-  //       value: "2019/10/14"
-  //     },
-  //     thing8: {
-  //       value: "kk"
-  //     },
-
-
-  //   },
-  //   templateId: 'UgxSFEgfxASQgj6E1IW_vLyQu07aasNidkbeQqHq-Ig', //模板id
-  //   miniprogramState: 'developer'
-  // })
-
-  // return '推送成功'
+      }
+  } 
+ }
+  return '推送成功和自动签到'
 }
 
 function getYYMMDD(d){
